15ddad843cec1c8d55452764e0872016c4c62900
Siela <boyko.sofania@gmail.com>
2025-12-08 11:23:34 +0100

    hw11

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+// src/js/main.js
+import { getImagesByQuery } from './pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const input = form.querySelector('input[name="search-text"]');
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+  const query = input.value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search query!',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+
+    if (!data.hits || data.hits.length === 0) {
+      iziToast.info({
+        title: 'No results',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
+
+    createGallery(data.hits);
+  } catch (error) {
+    iziToast.error({ title: 'Error', message: error.message });
+  } finally {
+    hideLoader();
+  }
+});
